6c7f27f4f0a4549025a809e142e05c52f5e631ea
ip613s07 <slava1138@gmail.com>
2020-12-19 00:33:54 +0700

    Add Apply button in inspector

--- Builder3DTestTask/Assets/Scripts/GridController.cs
+++ Builder3DTestTask/Assets/Scripts/GridController.cs
@@ -1,107 +1,30 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
-[ExecuteAlways]
-public class GridController : MonoBehaviour
+[CustomEditor(typeof(GridGenerator))]
+public class TreeGeneratorController : Editor
 {
 
-    [SerializeField]
-    private int xGridSize = 5;
-    [SerializeField]
-    private int zGridSize = 5;
+    GridGenerator TG;
 
-    [SerializeField]
-    private int xCellSize = 1;
-    [SerializeField]
-    private int zCellSize = 1;
-
-    [SerializeField]
-    private GameObject Tile = null;
-
-    [SerializeField]
-    private List<List<GameObject>> Tiles = null;
-
-    [SerializeField]
-    private bool isApply = false;
-
-    void Start()
+    private void OnEnable()
     {
+
+        TG = (GridGenerator)target;
 
     }
 
-    void Update()
+    override public void OnInspectorGUI()
     {
 
-        if (isApply)
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Apply"))
         {
 
-            isApply = false;
-
-            CreateGrid();
-
-        }
-
-    }
-
-    private void CreateGrid()
-    {
-
-        if (Tiles != null)
-        {            
-
-            for (int x = Tiles.Count - 1; x >= 0; --x)
-            {
-
-                for (int z = Tiles[x].Count - 1; z >= 0; --z)
-                {
-
-#if UNITY_EDITOR
-
-                    DestroyImmediate(Tiles[x][z]);
-
-#else
-
-                    Destroy(Tiles[x][z]);
-
-#endif
-
-                    Tiles[x].RemoveAt(z);
-
-                }
-
-                Tiles.RemoveAt(x);
-
-            }
-
-        }
-        else
-        {
-
-            Tiles = new List<List<GameObject>>();
-
-        }
-
-        for (int x = 0; x < xGridSize; ++x)
-        {
-
-            Tiles.Add(new List<GameObject>());
-
-            for (int z = 0; z < zGridSize; ++z)
-            {
-
-                Vector3 position = new Vector3(transform.position.x + (-(xGridSize / 2) + x) * xCellSize,
-                    0, transform.position.z + (-(zGridSize / 2) + z) * zCellSize);
-
-                GameObject t = Instantiate(Tile, position, Quaternion.identity, transform);
-
-                t.transform.localScale = new Vector3(xCellSize, 1, zCellSize);
-
-                Tiles[x].Add(t);
-
-            }
+            TG.GenerateGrid();
 
         }
 
